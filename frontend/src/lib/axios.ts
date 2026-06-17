b53fdd14305f6d083f319fbf0d51c8e8aa6dd1ad
Stephen Palanisamy Danial <stephen.palanisamy@srmtech.com>
2026-06-17 19:48:08 +0530

    Use Render API URL

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://172.16.13.152:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
